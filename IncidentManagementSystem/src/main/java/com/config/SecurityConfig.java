76da469e801df9b336ee412b85dc8cc97a5085ac
kashikaryash <kashikaryash@gmail.com>
2025-12-04 19:42:28 +0530

    done with this

--- IncidentManagementSystem/src/main/java/com/config/SecurityConfig.java
+++ IncidentManagementSystem/src/main/java/com/config/SecurityConfig.java
@@ -78,7 +78,7 @@ public class SecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowedOrigins(List.of(
-            "http://localhost:5173",
+            "https://incident-management-frontend.vercel.app/",
             "http://127.0.0.1:5173",
             "http://localhost:3000"
         ));
